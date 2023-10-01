027085fbbdc1deccb77bb99e1e4c601bdea13214
Vsevolod Vaskin <vaskin.2003@gmail.com>
2023-10-01 18:17:22 +0300

    Delete cmake-build-debug from git

--- cmake-build-debug/third_party/cryptominisat/scripts/fuzz/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vsevolod/spbu/Circuit-SAT-solver/third_party/cryptominisat/scripts/fuzz
-# Build directory: /home/vsevolod/spbu/Circuit-SAT-solver/cmake-build-debug/third_party/cryptominisat/scripts/fuzz
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- cmake-build-debug/third_party/cryptominisat/utils/minimal_cms/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/vsevolod/spbu/Circuit-SAT-solver/third_party/cryptominisat/utils/minimal_cms
-# Build directory: /home/vsevolod/spbu/Circuit-SAT-solver/cmake-build-debug/third_party/cryptominisat/utils/minimal_cms
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
